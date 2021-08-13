28785f0e81e6374d68216c77901b553cdfc45236
Thor Whalen <thorwhalen1@gmail.com>
2021-08-13 10:56:16 +0100

    0.0.5:

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # swipe
 
 Swipe through data once, with a comb that will pick up the points you're looking for.
@@ -7,14 +6,112 @@ Think single pass search.
 
 Think k nearest neighbors, but brute force, but with multiple targets.
 
+Extremely light weight: Pure python -- no other dependencies.
+
 # Example
 
-```pydocstring
->>> import swipe
->>> swipe.highest_score_swipe([5,3,7,2,9,7], k=2)
-[(7, 7), (9, 9)]
->>> swipe.highest_score_swipe(iter([5,3,7,2,9,7]), score_of=lambda x: x % 3, k=2)
-[(2, 2), (2, 5)]
+The main function of `swipe` is `highest_score_swipe`. 
+The other objects are there to support you in making your own kind of
+swiping function. 
+
+What `highest_score_swipe` does is 
+get the k items from the iterable `it` who score the highest with `score_of`.
+(think of `score_of` an inverse of `key` argument of the `sorted` python 
+function).
+
+As far as the output is concerned, you can acheive something about the same with
+
+```
+highest_score_swipe(it, score_of, k, output)
 ```
 
-That, but multidimensional and meant to be used with big data iterators.
+as with
+
+```
+output(sorted(it, key=score_of, reverse=True)[:k])
+```
+
+(With slightly different output functions)
+
+The difference is that in the last one,
+- you have to fit all of the data in memory
+- you have to sort all of the data
+
+But to get the top `k` elements you don't have to. You just have to scan though
+the data once while maintaining a list of the top items.
+So when there's a lot of data, `highest_score_swipe` will save you both memory and
+computation.
+
+```python
+>>> from swipe import highest_score_swipe
+>>>
+>>> data = [('Christian', 12), ('Seb', 88), ('Thor', 27), ('Sylvain', 42)]
+```
+
+Let's see what you get out of the box (i.e. only specifying what's required, 
+using defaults for all the rest).
+We'll `iter(data)` just to this once to show that data only has to be iterable.
+
+```python
+>>> highest_score_swipe(iter(data))
+[(('Thor', 27), ('Thor', 27))]
+```
+
+Now, out of the box, you don't get much, and looks a bit strange. 
+Reason is if you don't specify `k` you just get the
+top item, and if you don't specify what score should be used to measure the "top",
+it'll just use python's default comparison operator which here brings
+`('Thor', 27)` on the top because it's lexicographically the last. 
+
+And why is `('Thor', 27)` repeated twice? 
+Because it acts both as a score (the first) and a data item (the second).
+
+Where it becomes interesting (and useful) is when you specify what score function 
+it should use. So let's.
+
+```python
+>>> length_of_name = lambda x: len(x[0])
+>>> by_age = lambda x: x[1]
+>>> highest_score_swipe(data, by_age)
+[(88, ('Seb', 88))]
+>>> highest_score_swipe(data, length_of_name)
+[(9, ('Christian', 12))]
+>>> highest_score_swipe(data, length_of_name, k=2)
+[(7, ('Sylvain', 42)), (9, ('Christian', 12))]
+```
+
+Now let's see about that `output` argument. 
+It's used to specify how you want the result to be processed before returning.
+
+```python
+>>> highest_score_swipe(data, length_of_name, k=2, output='top_tuples')
+[(9, ('Christian', 12)), (7, ('Sylvain', 42))]
+>>> highest_score_swipe(data, length_of_name, k=2, output='items')
+[('Sylvain', 42), ('Christian', 12)]
+>>> highest_score_swipe(data, length_of_name, k=2, output='scores')
+[7, 9]
+>>> highest_score_swipe(data, length_of_name, k=2, output='top_score_items')
+[('Christian', 12), ('Sylvain', 42)]
+```
+
+You can also specify a custom function:
+
+```python
+>>> highest_score_swipe(
+...     data, length_of_name, k=2,
+...     output=lambda km: [f"{name} (whose name has {score} letters), is {age}" for score, (name, age) in km]
+... )
+['Sylvain (whose name has 7 letters), is 42', 'Christian (whose name has 9 letters), is 12']
+```
+
+What if you wanted the indices (that is, the integer indexing the data) of the top 2
+as your output? Here's a recipe for that:
+
+```python
+>>> highest_score_swipe(
+...     enumerate(data),  # enumerate the data to get a (i, item) iterator
+...     lambda x: length_of_name(x[1]),  # apply your scoring function to the item
+...     k=2,
+...     output=lambda km: [x[1][0] for x in km]  # extract the indices
+... )
+```
--- swipe/__init__.py
+++ swipe/__init__.py
@@ -1,5 +1,12 @@
+"""
+Swipe through data once, with a comb that will pick up the points you're looking for.
+
+Think single pass search.
+
+Think k nearest neighbors, but brute force, but with multiple targets.
+
+"""
 from heapq import heappushpop, heappush
-from numpy import array, argsort
 
 
 class KeepMaxK(list):
@@ -25,8 +32,7 @@ class KeepMaxUnikK(object):
             self.min_val_items.push((val, item))
 
     def items_sorted(self):
-        dists, items = list(zip(*self.min_val_items))
-        return array(items)[argsort(dists)]
+        return [x[1] for x in sorted(self.min_val_items, key=lambda x: x[0])]
 
 
 class KeepMinK(list):
@@ -68,43 +74,134 @@ class HighestScoreSwipe(object):
         pass
 
 
-def highest_score_swipe(it, score_of=None, k=1, info_of=None, output=None):
-    """
-    >>> import swipe
-    >>> swipe.highest_score_swipe([5,3,7,2,9,7], k=2)
-    [(7, 7), (9, 9)]
-    >>> swipe.highest_score_swipe(iter([5,3,7,2,9,7]), score_of=lambda x: x % 3, k=2)
-    [(2, 2), (2, 5)]
+def highest_score_swipe(it, score_of=None, k=1, output=None):
+    """Get the k items from the iterable `it` who score the highest with `score_of`.
+    (think of `score_of` an inverse of `key` argument of the `sorted` python function).
+
+    As far as the output is concerned, you can acheive something about the same with
+
+    ```
+    highest_score_swipe(it, score_of, k, output)
+    ```
+
+    as with
+
+    ```
+    output(sorted(it, key=score_of, reverse=True)[:k])
+    ```
+
+    (With slightly different output functions)
+
+    The difference is that in the last one,
+    - you have to fit all of the data in memory
+    - you have to sort all of the data
+
+    But to get the top `k` elements you don't have to. You just have to scan though
+    the data once while maintaining a list of the top items.
+    So when there's a lot of data, `highest_score_swipe` will save you both memory and
+    computation.
+
+    >>> from swipe import highest_score_swipe
+    >>>
+    >>> data = [('Christian', 12), ('Seb', 88), ('Thor', 27), ('Sylvain', 42)]
+
+    Let's see what you get out of the box (i.e. only specifying what's required,
+    using defaults for all the rest).
+    We'll `iter(data)` just to this once to show that data only has to be iterable.
+
+    >>> highest_score_swipe(iter(data))
+    [(('Thor', 27), ('Thor', 27))]
+
+    Now, out of the box, you don't get much, and looks a bit strange.
+    Reason is if you don't specify `k` you just get the
+    top item, and if you don't specify what score should be used to measure the "top",
+    it'll just use python's default comparison operator which here brings
+    `('Thor', 27)` on the top because it's lexicographically the last.
+
+    And why is `('Thor', 27)` repeated twice?
+    Because it acts both as a score (the first) and a data item (the second).
+
+    Where it becomes interesting (and useful) is when you specify what score function
+    it should use. So let's.
+
+    >>> length_of_name = lambda x: len(x[0])
+    >>> by_age = lambda x: x[1]
+    >>> highest_score_swipe(data, by_age)
+    [(88, ('Seb', 88))]
+    >>> highest_score_swipe(data, length_of_name)
+    [(9, ('Christian', 12))]
+    >>> highest_score_swipe(data, length_of_name, k=2)
+    [(7, ('Sylvain', 42)), (9, ('Christian', 12))]
+
+    Now let's see about that `output` argument.
+    It's used to specify how you want the result to be processed before returning.
+
+    >>> highest_score_swipe(data, length_of_name, k=2, output='top_tuples')
+    [(9, ('Christian', 12)), (7, ('Sylvain', 42))]
+    >>> highest_score_swipe(data, length_of_name, k=2, output='items')
+    [('Sylvain', 42), ('Christian', 12)]
+    >>> highest_score_swipe(data, length_of_name, k=2, output='scores')
+    [7, 9]
+    >>> highest_score_swipe(data, length_of_name, k=2, output='top_score_items')
+    [('Christian', 12), ('Sylvain', 42)]
+
+    You can also specify a custom function:
+
+    >>> highest_score_swipe(
+    ...     data, length_of_name, k=2,
+    ...     output=lambda km: [f"{name} (whose name has {score} letters), is {age}" for score, (name, age) in km]
+    ... )
+    ['Sylvain (whose name has 7 letters), is 42', 'Christian (whose name has 9 letters), is 12']
+
+    What if you wanted the indices (that is, the integer indexing the data) of the top 2
+    as your output? Here's a recipe for that:
+
+    >>> highest_score_swipe(
+    ...     enumerate(data),  # enumerate the data to get a (i, item) iterator
+    ...     lambda x: length_of_name(x[1]),  # apply your scoring function to the item
+    ...     k=2,
+    ...     output=lambda km: [x[1][0] for x in km]  # extract the indices
+    ... )
+
+    :param it: An iterable of data items
+    :param score_of: The function that will be applied to the items to get a score
+    :param k: The number of highest score items to return
+    :param output: How to prostprocess what's returned.
+    By default it will be a KeepMaxK instance, which is a list/heap that keeps only top
+    (score, item) pairs. But you can ask for
+        - output == "top_tuples": Get the same, but as a (descending order) sorted list
+        (a KeepMaxK maintains a list of top items, but not in sorted order!)
+        - output == "items": Only the items
+        - output == "scores": Only the scores
+        - output == "top_score_items": Only items, sorted by descending score order
+        - if not, output is a callable that will be called on your output
+    :return:
+
+
     """
     if score_of is None:
         score_of = lambda x: x
 
+    if output is None:
+        output = lambda km: km
+    elif isinstance(output, str):
+        if output == "top_tuples":
+            output = lambda km: sorted(km, reverse=True)
+        elif output == "items":
+            output = lambda km: [x[1] for x in km]
+        elif output == "scores":
+            output = lambda km: [x[0] for x in km]
+        elif output == "top_score_items":
+            output = lambda km: [
+                x[1] for x in sorted(km, key=lambda x: x[0], reverse=True)
+            ]
+
+    if not callable(output):
+        raise ValueError("Unrecognized output: ".format(output))
+
     km = KeepMaxK(k=k)
 
-    if info_of is None:
-        for x in it:
-            km.push((score_of(x), x))
-    else:
-        if info_of == 'idx':
-            for i, x in enumerate(it):
-                km.push((score_of(x), i))
-        else:
-            assert callable(info_of), "info_of needs to be a callable (if not None or 'idx')"
-            for x in it:
-                km.push((score_of(x), info_of(x)))
+    for x in it:
+        km.push((score_of(x), x))
 
-    if output is None:
-        return km
-    elif isinstance(output, str):
-        if output == 'top_tuples':
-            return sorted(km, reverse=True)
-        elif output == 'items':
-            return [x[1] for x in km]
-        elif output == 'scores':
-            return [x[0] for x in km]
-        elif output == 'top_score_items':
-            return [x[1] for x in sorted(km, key=lambda x: x[0])]
-        else:
-            raise ValueError("Unrecognized output: ".format(output))
-    else:
-        return km
+    return output(km)
